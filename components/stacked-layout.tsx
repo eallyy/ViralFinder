acafff79e5299c058af18057ad5fcff7cde53f12
Emir <eallyy@icloud.com>
2025-08-03 18:17:06 +0300

    build fix

--- components/stacked-layout.tsx
+++ components/stacked-layout.tsx
@@ -23,7 +23,7 @@ function CloseMenuIcon() {
 export function StackedLayout({
   navbar,
   children,
-}: React.PropsWithChildren<{ navbar: React.ReactNode; sidebar: React.ReactNode }>) {
+}: React.PropsWithChildren<{ navbar: React.ReactNode }>) {
 
   return (
     <div className="relative isolate flex min-h-svh w-full flex-col bg-white lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
